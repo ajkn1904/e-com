854381b58a6d6e8e61044ad92f909b02388e56e9
ajkn1904 <ajknishat@gmail.com>
2023-02-17 20:26:21 +0600

    update

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SideNav = ({data}) => {
+const SideNav = ({ data }) => {
+
+
+    const [isActive, setIsActive] = useState({});
+
+    const handleClick = (id) => {
+        setIsActive(prevActiveButtons => ({
+            [id]: !prevActiveButtons[id]
+        }));
+    };
+
+
 
 
     return (
         //<div className='hidden md:block w-[247px]'>
         <div className='hidden md:block  w-[247px] h-[79vh] scrollbar-thin scrollbar-thumb-[#5DA9E9] scrollbar-track-[#EDEDED] overflow-y-scroll scrollbar-thumb-rounded-full mt-[21px] top-28'>
             {
-            data.map(categories => <button key={categories.cat_id} className='w-[207px] h-[42px] text-[#737D94] text-left bg-white hover:bg-[#5DA9E9] hover:text-white active:bg-[#5DA9E9] active:text-white border-0 px-[10px] py-10 ml-5 mr-[19.81px] my-[10px] flex justify-start items-center text-base font-extrabold rounded-[10px] transition-colors duration-200 ease-in-out'>{categories.category}</button>)
+                data.map(categories =>
+                    <a href={`#${categories.cat_id}`}
+                        key={categories.cat_id}
+                        className={`active-btn w-[207px] h-[42px] text-[#737D94] text-left bg-white border-0 px-[10px] py-10 ml-5 mr-[19.81px] my-[10px] flex justify-start items-center text-base font-extrabold rounded-[10px] transition-colors ease-in-out hover:bg-[#5DA9E9] hover:text-white ${isActive[categories.cat_id] ? 'bg-[#5DA9E9] text-white' : ''}`}
+                        onClick={() => handleClick(categories.cat_id)}
+                    >
+                        {
+                            categories.category
+                        }
+                    </a>
+                )
             }
-        </div>
+        </div >
     );
 };
 
--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const TopNav = ({ data }) => {
+
+    const [isActive, setIsActive] = useState({});
+
+    const handleClick = id => {
+        setIsActive(prevActiveButtons => ({
+           [id]: !prevActiveButtons[id]
+          }));
+    };
+
+
+
+
     return (
         <div className='md:hidden flex gap-[15px] whitespace-nowrap overflow-auto scrollbar-hide ml-7'>
             {
                 data.map(categories =>
-                    <div key={categories.cat_id} className='rounded-md text-[#737D94] active:text-[#5DA9E9] hover:text-[#5DA9E9]' >
-                       
-                       
-                        <div className='bg-[#EDEDED] active:bg-[#5DA9E9] active:content-white h-12 w-12 rounded-[5px] flex items-center justify-center'>
+                    <div key={categories.cat_id} className='active-btn rounded-md text-[#737D94] hover:text-[#5DA9E9]'>
+                        <a href={`#${categories.cat_id}`}
+                        onClick={() => handleClick(categories.cat_id)}>
 
-                            <img title={categories.category} src={categories.category_img} alt={categories.category} className='w-[19.16px] h-[19.16px]' />
+                            <div className={`bg-[#EDEDED] hover:bg-[#5DA9E9] active:content-white h-12 w-12 rounded-[5px] flex items-center justify-center ${isActive[categories.cat_id] ? 'bg-[#5DA9E9]' : ''} `}>
 
-                        </div>
+                                <img title={categories.category} src={categories.category_img} alt={categories.category} className='w-[19.16px] h-[19.16px]' />
+
+                            </div>
 
 
-                        <p title={categories.category} className='w-12 truncate font-extrabold text-xs mt-[5px] mb-[15px]'>{categories.category}</p>
+                            <p title={categories.category} className={`w-12 truncate font-extrabold text-xs mt-[5px] mb-[15px] ${isActive[categories.cat_id] ? 'text-[#5DA9E9]' : ''} `}>{categories.category}</p>
+
+                        </a>
+
 
 
                     </div>
